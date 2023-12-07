6d854028cb78ac5c2c1dce0a6bc12677fbdfa9f9
jefromyers <jefmyers@gmail.com>
2023-12-06 19:53:05 -0500

    Carve out space for testing

--- exercises/test_state_machine_game.py
+++ exercises/test_state_machine_game.py
@@ -6,14 +6,8 @@ def test_smokem_if_you_got_em():
     assert True == True
 
 
-def test_message_with_data():
+def test_guess_message_with_data():
     guess_message = Message.GUESS.with_data({"word": "apple"})
 
-    assert guess_message.label == Message.GUESS.value
-    assert guess_message.data == {"word": "apple"}
-
-
-def test_message_enum_value():
-    assert Message.GUESS.value == "guess"
-    assert Message.REQUEST.value == "request"
-    assert Message.CONFIRM.value == "confirm"
+    assert guess_message == Message.GUESS
+    assert guess_message.data["word"] == "apple"
